f4beed46c1ac749fa3a1509f2c32f935960fc665
BLKelsey <blkelsey@gmail.com>
2026-01-27 16:04:21 -0500

    Filled in text boxes: added time.sleep() to see actions

--- Fill-in Text Boxes.py
+++ Fill-in Text Boxes.py
@@ -7,8 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
-# Already on Text Box page
 driver.get("https://demoqa.com/text-box")
+time.sleep(1)
 
 # -------------------------------------------------
 # Enter text into Full Name field
@@ -18,6 +18,7 @@ full_name_field = wait.until(
 )
 full_name_field.send_keys("Brian Kelsey")
 print("Entered Full Name.")
+time.sleep(1)
 
 # -------------------------------------------------
 # Enter text into Email field
@@ -27,6 +28,7 @@ email_field = wait.until(
 )
 email_field.send_keys("blkelsey@gmail.com")
 print("Entered Email.")
+time.sleep(1)
 
 # -------------------------------------------------
 # Enter text into Current Address field
@@ -36,6 +38,7 @@ current_address_field = wait.until(
 )
 current_address_field.send_keys("123 Main Street")
 print("Entered Current Address.")
+time.sleep(1)
 
 # -------------------------------------------------
 # Enter text into Permanent Address field
@@ -45,11 +48,14 @@ permanent_address_field = wait.until(
 )
 permanent_address_field.send_keys("123 Oak Avenue")
 print("Entered Permanent Address.")
+time.sleep(1)
 
 # -------------------------------------------------
 # Click Submit button
 # -------------------------------------------------
 submit_button = driver.find_element(By.ID, "submit")
+driver.execute_script("arguments[0].scrollIntoView(true);", submit_button)
+time.sleep(1)
 submit_button.click()
 print("Clicked Submit button.")
 
