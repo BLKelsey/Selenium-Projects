9fd097b62b4c30502a8539fc9dd437bff11db7ee
BLKelsey <blkelsey@gmail.com>
2026-02-26 15:30:29 -0500

    removed time.sleep functions

--- Click Link.py
+++ Click Link.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
  
 
 driver = webdriver.Chrome()
@@ -14,8 +13,6 @@ driver.get("https://www.qaplayground.com/practice")
 link = wait.until(
     EC.element_to_be_clickable((By.LINK_TEXT, "Click")))
 
-time.sleep(2)  # Just to visually confirm the link is ready
-
 # Click the link
 print("Clicking the link...")
 link.click()
@@ -25,9 +22,8 @@ print("Link clicked.")
 
 home_btn = wait.until(
     EC.element_to_be_clickable((By.ID, "button-color")))
-time.sleep(2)  # Just to visually confirm the link is ready
 home_btn.click()
-time.sleep(2)  # Just to visually confirm the link is ready
+
 
 print("Navigated back to home page.")
 
