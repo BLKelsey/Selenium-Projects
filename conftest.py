3f91b97f0c39428101ba0b0f617054bbae65114c
BLKelsey <blkelsey@gmail.com>
2026-03-01 13:10:50 -0500

    added fixures for Bearer token test

--- conftest.py
+++ conftest.py
@@ -7,3 +7,18 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
+    
+@pytest.fixture(scope="session")
+def base_url():
+   return "https://fakestoreapi.com/"
+ 
+@pytest.fixture(scope="session")
+def endpoint(base_url):
+    return f"{base_url}/auth/login"
+  
+@pytest.fixture(scope="session")
+def credentials():
+    return {
+        "username": "mor_2314",
+        "password": "83r5^_"
+    }
